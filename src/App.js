26ab9cc1543abe17a2f01aabb88d99e0ef236e7d
Steven Than <steventhan11@gmail.com>
2017-04-20 23:53:48 -0400

    incorporated material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
-import { Navbar, Footer } from "react-bootstrap";
+import Login from "./components/Registration/Login";
+import theme from "./theme";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import Drawer from "material-ui/Drawer";
 import "./App.css";
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {loggedIn: false};
+  }
+
   render() {
     return (
-      <div>
-        <Footer />
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+        <div className="container">
+          {this.state.loggedIn ? <AppBar title="NU Web Planner" /> : <Login />}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }

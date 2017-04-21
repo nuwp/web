241fdf8ffdbd3b0ae4f187b018afca5ad8dc1890
Steven Than <steventhan11@gmail.com>
2017-04-20 23:51:48 -0400

    changed theme color to red, will need revisit

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import {
-  cyan500, cyan700,
+  red700, red900,
   pinkA200,
   grey100, grey300, grey400, grey500,
   white, darkBlack, fullBlack,
@@ -11,8 +11,8 @@ export default {
   spacing: spacing,
   fontFamily: "Roboto, sans-serif",
   palette: {
-    primary1Color: cyan500,
-    primary2Color: cyan700,
+    primary1Color: red700,
+    primary2Color: red900,
     primary3Color: grey400,
     accent1Color: pinkA200,
     accent2Color: grey100,
@@ -22,7 +22,7 @@ export default {
     canvasColor: white,
     borderColor: grey300,
     disabledColor: fade(darkBlack, 0.3),
-    pickerHeaderColor: cyan500,
+    pickerHeaderColor: red700,
     clockCircleColor: fade(darkBlack, 0.07),
     shadowColor: fullBlack,
   },

a5182c905af302f47fb2fddf6e58ddc1c33d1f17
Steven Than <steventhan11@gmail.com>
2017-04-21 23:44:43 -0400

    removed style of .container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
-import Login from "./components/Registration/Login";
+import injectTapEventPlugin from "react-tap-event-plugin";
 import theme from "./theme";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import AppBar from "material-ui/AppBar";
-import Drawer from "material-ui/Drawer";
 import "./App.css";
+import Login from "./components/Registration/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
+injectTapEventPlugin();
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {loggedIn: false};
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    this.setState({
+      loggedIn: true
+    });
   }
 
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
         <div className="container">
-          {this.state.loggedIn ? <AppBar title="NU Web Planner" /> : <Login />}
+          {this.state.loggedIn ? <Dashboard /> : <Login login={this.login} />}
         </div>
       </MuiThemeProvider>
     );

d46ef5892011a39186183287f73264cff88502bd
Steven Than <steventhan11@gmail.com>
2017-05-01 16:06:31 -0700

    create Main container that represent the app at logged in state

---
+++ src/containers/Main.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import AppBar from "material-ui/AppBar";
+import SideNav from "../components/menu/SideNav";
+import { toggleSideNav, closeSideNav } from "../actions/sidenavActions";
+
+const mapStateToProps = (store) => ({open: store.sidenav.open});
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleSideNav = this.toggleSideNav.bind(this);
+    this.closeSideNav = this.closeSideNav.bind(this);
+  }
+
+  toggleSideNav() {
+    this.props.dispatch(toggleSideNav());
+  }
+
+  closeSideNav() {
+    this.props.dispatch(closeSideNav());
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="NU Web Planner"
+          onLeftIconButtonTouchTap={this.toggleSideNav}
+        />
+        <SideNav
+          closeSideNav={this.closeSideNav}
+          sideNavOpen={this.props.open}
+        />
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Main);
